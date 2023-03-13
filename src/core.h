4b5120a69534e25c13b05aac8eeb1c04d7e969de
lightweigh <827907347@qq.com>
2023-03-13 20:00:27 +0800

    done generating core.dll

--- src/core.h
+++ src/core.h
@@ -1,4 +1,12 @@
+#ifdef CORE_DLL
+#define CORE_EXPOSE extern "C" __declspec(dllexport)
+#else
+#define CORE_EXPOSE extern "C" __declspec(dllimport)
+#endif
 
+CORE_EXPOSE
 int gen_chains_all(const char* words[], int len, char* result[]); 
+CORE_EXPOSE
 int gen_chain_word(const char* words[], int len, char* result[], char head, char tail, char skip, bool enable_loop);
+CORE_EXPOSE
 int gen_chain_char(const char* words[], int len, char* result[], char head, char tail, char skip, bool enable_loop);
